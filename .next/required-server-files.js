a993f3145a1a718ade3c445849588a1061facb82
carlos alejandro ibarra ariza <dev.ventogroup@gmail.com>
2026-07-13 16:39:39 -0500

    Actualización

--- .next/required-server-files.js
+++ .next/required-server-files.js
@@ -140,7 +140,7 @@ self.__SERVER_FILES_MANIFEST={
       "isExperimentalCompile": false
     }
   },
-  "appDir": "C:\\Vento-Projects\\vento-pulso",
+  "appDir": "C:\\Users\\User\\devVentoGroup\\vento-pulso",
   "relativeAppDir": "",
   "files": [
     ".next\\routes-manifest.json",
--- .next/types/validator.ts
+++ .next/types/validator.ts
@@ -63,6 +63,15 @@ type LayoutConfig<Route extends LayoutRoutes = LayoutRoutes> = {
   type __Unused = __Check
 }
 
+// Validate ../../src/app/sales-imports/page.tsx
+{
+  type __IsExpected<Specific extends AppPageConfig<"/sales-imports">> = Specific
+  const handler = {} as typeof import("../../src/app/sales-imports/page.js")
+  type __Check = __IsExpected<typeof handler>
+  // @ts-ignore
+  type __Unused = __Check
+}
+
 // Validate ../../src/app/salon/page.tsx
 {
   type __IsExpected<Specific extends AppPageConfig<"/salon">> = Specific
